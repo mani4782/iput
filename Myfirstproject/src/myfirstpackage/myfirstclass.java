588d5e6ffaf9cbffa309b82d57dd8a38a1801a82
mani <vmanikumar3@gmail.com>
2022-01-12 00:01:37 +0000

    this is my first push

---
+++ Myfirstproject/src/myfirstpackage/myfirstclass.java
@@ -0,0 +1,13 @@
+package myfirstpackage;
+
+public class myfirstclass {
+
+	public myfirstclass() {
+	}
+
+	public static void main(String[] args) {
+ 
+		System.out.println( "this is my first code");	
+	}
+
+}
