906afb98fa643520e28ec4855a48cddfc00db366
Balu <bthagarapu@gmail.com>
2022-01-12 01:39:34 +0000

    this is updated

---
+++ myfirstclass.java
@@ -0,0 +1,26 @@
+
+package myfirstpackage;
+
+public class myfirstclass {
+
+	public myfirstclass() {
+	}
+
+	public static void main(String[] args) {
+ 
+		System.out.println( "this is my first codee");	
+        System.out.println("my sec num");
+		System.out.println("this is balraj branch taken from master code base");
+		System.out.println("this is mani branch taken from the balraj branch");
+		System.out.println("this is test");
+	}
+
+}
+
+
+
+
+
+
+
+
