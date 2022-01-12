4987c986e324d3707255c404015e3983530df84d
mani <vmanikumar3@gmail.com>
2022-01-12 00:30:12 +0000

    hi

--- Myfirstproject/src/myfirstpackage/myfirstclass.java
+++ Myfirstproject/src/myfirstpackage/myfirstclass.java
@@ -8,7 +8,7 @@ public class myfirstclass {
 	public static void main(String[] args) {
  
 		System.out.println( "this is my first codee");	
-       
+        System.out.println("my sec num");
 		
 	}
 
