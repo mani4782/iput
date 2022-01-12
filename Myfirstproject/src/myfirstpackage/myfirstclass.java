8e37000b86cea601f2b1d8b80841e3701c2bc555
mani <vmanikumar3@gmail.com>
2022-01-12 00:21:23 +0000

    my

--- Myfirstproject/src/myfirstpackage/myfirstclass.java
+++ Myfirstproject/src/myfirstpackage/myfirstclass.java
@@ -7,7 +7,7 @@ public class myfirstclass {
 
 	public static void main(String[] args) {
  
-		System.out.println( "this is my first code");	
+		System.out.println( "this is my first codee");	
        
 		
 	}
